f16aad8913c0c88eb86f7aca29c4609cc0fe9c44
Minjeong Sunny Kim <sunnykim@minjeongs-air.wifi.local.cmu.edu>
2024-03-31 16:05:38 -0400

    added faulty upload function

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, send_file, request
-
+#import processing_file_name
 app = Flask(__name__)
 
 
@@ -16,8 +16,18 @@ def chipseq():
 def chipseq_sample():
     return send_file(F'sample_data/ChIP-seq/{request.args.get("sample")}')
 
-@app.route('/database/null_model')
-def null_model():
-    return send_file('sample_data/Null/CommonSNP_MAFmatch')
+# @app.route('/database/null_model')
+# def null_model():
+#     return send_file('sample_data/Null/CommonSNP_MAFmatch')
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    uploaded_file = request.files['file']
+    print("Uploaded file:", uploaded_file)
+    if uploaded_file.filename != '':
+        uploaded_file.save("test")
+    #results = processing_file_name.some_function(uploaded_file)
+    #return results
+    
 
 # TODO: Will send dbSNP data file, but currently too big to fit in GitHub.
